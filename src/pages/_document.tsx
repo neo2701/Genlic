dae83d9dc9371404b481def682d5d4f9e756c70b
neo2701 <nindyapramana123@gmail.com>
2022-12-21 22:25:44 +0700

    added relicbrowser and vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
+import { AnalyticsWrapper } from "@/components/analytics";
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
-
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
 
@@ -9,6 +9,7 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
 
+        <script src="/assets/js/relicbrowser.js"></script>
         <link type="image/png" sizes="16x16" rel="icon" href="/assets/img/favicon/icons8-box-important-glyph-neue-16.png"></link>
         <link type="image/png" sizes="32x32" rel="icon" href="/assets/img/favicon/icons8-box-important-glyph-neue-32.png"></link>
         <link type="image/png" sizes="96x96" rel="icon" href="/assets/img/favicon/icons8-box-important-glyph-neue-96.png"></link>
@@ -18,6 +19,7 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
+          <AnalyticsWrapper />
         </body>
       </Html>
     );
